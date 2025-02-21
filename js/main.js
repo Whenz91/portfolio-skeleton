9ddd7bb1cc561932a98ed3d90bae6c2438e1e938
Mikó Zoltán <mikozoltan91@gmail.com>
2025-02-21 15:44:49 +0100

    finish about page and add GSAP scroll animations

--- js/main.js
+++ js/main.js
@@ -20,9 +20,103 @@ function renderSideMenuContent(skill) {
     sideMenuContent.innerHTML = data[0][skill]['description'];
 }
 
-window.addEventListener('load', async () => {
+document.addEventListener("DOMContentLoaded", async (event) => {
+    gsap.registerPlugin(ScrollTrigger);
     skills = await getData();
-})
+
+    const fadeInFromLeft = (selector, trigger) => {
+        gsap.fromTo(selector, 
+            { opacity: 0, x: -100 },
+            {
+                opacity: 1, x: 0,
+                duration: 1,
+                ease: "power2.out",
+                scrollTrigger: {
+                    trigger: trigger,
+                    start: "top 80%",
+                    toggleActions: "play none none none"
+                }
+            }
+        );
+    };
+    
+    const fadeInFromRight = (selector, trigger) => {
+        gsap.fromTo(selector, 
+            { opacity: 0, x: 100 },
+            {
+                opacity: 1, x: 0,
+                duration: 1,
+                ease: "power2.out",
+                scrollTrigger: {
+                    trigger: trigger,
+                    start: "top 80%",
+                    toggleActions: "play none none none"
+                }
+            }
+        );
+    };
+    
+    const fadeInFromTop = (selector, trigger) => {
+        gsap.fromTo(selector, 
+            { opacity: 0, y: -200 },
+            {
+                opacity: 1, y: 0,
+                duration: 1,
+                ease: "power2.out",
+                scrollTrigger: {
+                    trigger: trigger,
+                    start: "top 80%",
+                    toggleActions: "play none none none"
+                }
+            }
+        );
+    };
+    
+    const fadeInFromBottom = (selector, trigger) => {
+        gsap.fromTo(selector, 
+            { opacity: 0, y: 500 },
+            {
+                opacity: 1, y: 0,
+                duration: 1,
+                ease: "power2.out",
+                scrollTrigger: {
+                    trigger: trigger,
+                    start: "top 80%",
+                    toggleActions: "play none none none"
+                }
+            }
+        );
+    };
+    
+    // Animációk alkalmazása
+    gsap.utils.toArray(".section-title").forEach(title => fadeInFromLeft(title, title));
+
+    if(document.querySelector('#about')) {
+        fadeInFromLeft('.gsap-box-1', '#about');
+        fadeInFromRight('.gsap-box-2', '#about');
+        fadeInFromTop('.gsap-box-3', '#project');
+        fadeInFromBottom('.gsap-box-4', '#project');
+        fadeInFromLeft('#skill .grid', '#skill');
+    }
+    if(document.querySelector('#experience')) {
+        fadeInFromLeft('.gsap-box-1', '#experience');
+        fadeInFromRight('.gsap-box-2', '#experience');
+
+        gsap.from(".gsap-boxes > div", {
+            opacity: 0,
+            y: 50,
+            duration: 1,
+            ease: "power2.out",
+            stagger: 0.3,
+            scrollTrigger: {
+                trigger: "#hobbie",
+                start: "top 80%",
+                toggleActions: "play none none none"
+            }
+        });
+    }
+    
+});
 
 document.querySelector('body').addEventListener('click', (e) => {
     if(e.target.classList.contains('js-mobile-menu-btn')) {
